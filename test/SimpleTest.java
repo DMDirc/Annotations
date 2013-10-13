26bb24f85a8c85e2694e7b20971f35a38f73485e
Chris Smith <chris@dmdirc.com>
2013-10-13 18:46:55 +0100

    Add Ivy.
    Change-Id: Ibdeb4ecee6da3238ba4ff3951abc919eba5d792e

--- test/SimpleTest.java
+++ test/SimpleTest.java
@@ -2,8 +2,9 @@ import com.dmdirc.util.annotations.factory.Factory;
 import com.dmdirc.util.annotations.factory.Unbound;
 
 import java.util.List;
+import javax.lang.model.element.Modifier;
 
-@Factory
+@Factory(inject=true, providers=true, singleton=true, name="Flub", modifiers = {Modifier.ABSTRACT})
 public class SimpleTest {
 
     public SimpleTest(String foo, String bar, @SuppressWarnings("Foo") List<String> stuffs, @Unbound @Deprecated int meh) {}
